ba7c74221d14a083bcedc3d3b9f6b19ec829b6fa
Zane Schaffer <personal.zane@gmail.com>
2023-01-14 23:17:55 -0800

    add format flag : fixes #1

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-use std::{fs, path::PathBuf};
+use std::{fmt::Display, fs, path::PathBuf, str::FromStr};
 
 use anyhow::anyhow;
-use clap::{command, Parser};
+use clap::{command, Parser, ValueEnum};
 
-#[derive(Parser)]
+#[derive(Parser, Clone)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
     /// Input file to transcode
@@ -11,11 +11,15 @@ struct Cli {
     input: PathBuf,
 
     /// Output file to write
-    #[arg(value_name = "OUTPUT")]
-    output: PathBuf,
+    #[arg(value_name = "OUTPUT", required_unless_present = "format")]
+    output: Option<PathBuf>,
+
+    /// Output format
+    #[arg(value_enum, long, short)]
+    format: Option<FileFormat>,
 }
 
-#[derive(Copy, Clone, PartialEq, Eq)]
+#[derive(Copy, Clone, PartialEq, Eq, ValueEnum)]
 enum FileFormat {
     Json,
     Yaml,
@@ -23,12 +27,66 @@ enum FileFormat {
     Unknown,
 }
 
-struct IO {
+impl Display for FileFormat {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            FileFormat::Json => write!(f, "json"),
+            FileFormat::Yaml => write!(f, "yml"),
+            FileFormat::Toml => write!(f, "toml"),
+            FileFormat::Unknown => write!(f, "txt"),
+        }
+    }
+}
+
+impl FromStr for FileFormat {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "json" => Ok(Self::Json),
+            "yaml" | "yml" => Ok(Self::Yaml),
+            "toml" => Ok(Self::Toml),
+            _ => Ok(Self::Unknown),
+        }
+    }
+}
+
+trait IO {
+    fn path(&self) -> &PathBuf;
+    fn format(&self) -> &FileFormat;
+}
+
+struct Input {
     path: PathBuf,
     format: FileFormat,
 }
 
-impl IO {
+struct Output {
+    path: PathBuf,
+    format: FileFormat,
+}
+
+impl IO for Input {
+    fn path(&self) -> &PathBuf {
+        &self.path
+    }
+
+    fn format(&self) -> &FileFormat {
+        &self.format
+    }
+}
+
+impl IO for Output {
+    fn path(&self) -> &PathBuf {
+        &self.path
+    }
+
+    fn format(&self) -> &FileFormat {
+        &self.format
+    }
+}
+
+impl Input {
     fn new(path: PathBuf) -> Self {
         let format = match path.extension() {
             None => FileFormat::Unknown,
@@ -43,11 +101,46 @@ impl IO {
         Self { path, format }
     }
 }
+
+impl Output {
+    fn new(path: PathBuf, format: Option<FileFormat>) -> Self {
+        if let Some(format) = format {
+            Self { path, format }
+        } else {
+            let format = match path.extension() {
+                None => FileFormat::Unknown,
+                Some(os_str) => match os_str.to_str() {
+                    Some("json") => FileFormat::Json,
+                    Some("toml") => FileFormat::Toml,
+                    Some("yaml") | Some("yml") => FileFormat::Yaml,
+                    _ => FileFormat::Unknown,
+                },
+            };
+
+            Self { path, format }
+        }
+    }
+}
+
 pub fn start() -> anyhow::Result<()> {
     let cli = Cli::parse();
 
-    let input = IO::new(cli.input);
-    let output = IO::new(cli.output);
+    let input = Input::new(cli.clone().input);
+
+    let output_path = match cli.output {
+        None => {
+            let mut path = cli.clone().input;
+            path.set_extension(
+                cli.format
+                    .expect("If no output was given, we must have a format flag")
+                    .to_string(),
+            );
+            path
+        }
+        Some(path) => path,
+    };
+
+    let output = Output::new(output_path, cli.format);
 
     if input.format == output.format {
         return Err(anyhow!("Input format is the same as output format."));
@@ -67,37 +160,37 @@ pub fn start() -> anyhow::Result<()> {
     }
 }
 
-fn transcode(input: &IO, output: &IO) -> anyhow::Result<String> {
-    match (input.format, output.format) {
+fn transcode(input: &impl IO, output: &impl IO) -> anyhow::Result<String> {
+    match (input.format(), output.format()) {
         // Output YAML
         (FileFormat::Json, FileFormat::Yaml) => {
             let value =
-                serde_json::from_str::<serde_yaml::Value>(&fs::read_to_string(&input.path)?)?;
+                serde_json::from_str::<serde_yaml::Value>(&fs::read_to_string(input.path())?)?;
             Ok(serde_yaml::to_string(&value).unwrap())
         }
         (FileFormat::Toml, FileFormat::Yaml) => {
-            let value = toml::from_str::<serde_yaml::Value>(&fs::read_to_string(&input.path)?)?;
+            let value = toml::from_str::<serde_yaml::Value>(&fs::read_to_string(input.path())?)?;
             Ok(serde_yaml::to_string(&value).unwrap())
         }
 
         // Output TOML
         (FileFormat::Json, FileFormat::Toml) => {
-            let value = serde_json::from_str::<toml::Value>(&fs::read_to_string(&input.path)?)?;
+            let value = serde_json::from_str::<toml::Value>(&fs::read_to_string(input.path())?)?;
             Ok(toml::to_string(&value).unwrap())
         }
         (FileFormat::Yaml, FileFormat::Toml) => {
-            let value = serde_yaml::from_str::<toml::Value>(&fs::read_to_string(&input.path)?)?;
+            let value = serde_yaml::from_str::<toml::Value>(&fs::read_to_string(input.path())?)?;
             Ok(toml::to_string(&value).unwrap())
         }
 
         // Output JSON
         (FileFormat::Yaml, FileFormat::Json) => {
             let value =
-                serde_yaml::from_str::<serde_json::Value>(&fs::read_to_string(&input.path)?)?;
+                serde_yaml::from_str::<serde_json::Value>(&fs::read_to_string(input.path())?)?;
             Ok(serde_json::to_string(&value).unwrap())
         }
         (FileFormat::Toml, FileFormat::Json) => {
-            let value = toml::from_str::<serde_json::Value>(&fs::read_to_string(&input.path)?)?;
+            let value = toml::from_str::<serde_json::Value>(&fs::read_to_string(input.path())?)?;
             Ok(serde_json::to_string(&value).unwrap())
         }
 
